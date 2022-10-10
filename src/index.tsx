5b7cc4c6fb54a942b3270944794ef3a4a682b7bd
Kristin Galvin <kgalvin@nvidia.com>
2022-10-10 08:18:30 -0700

    update index

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,1 @@
-export * from "./Oscilloscope";
-export * from "./Slider";
-export * from "./Button";
-export * from "./PlayPauseAudio";
-export * from "./PlayPauseButton";
-export * from "./Spectrogram";
-export * from "./Switch";
-
 export * from "./utils";
