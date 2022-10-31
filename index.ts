2630387e0c344183b05db32576b57dcfb052de7a
blechdom <kgalvin@nvidia.com>
2022-10-31 16:10:44 +0000

    Deploying to gh-pages from  @ f9d2017541740fd77523f91919607a0354c144ac 🚀

---
+++ index.ts
@@ -0,0 +1,28 @@
+import "./webaudio-controls-module";
+import { DOMAttributes } from "react";
+import { WebAudioKnob } from "./webaudio-controls-module";
+import { WebAudioSlider } from "./webaudio-controls-module";
+import { WebAudioSwitch } from "./webaudio-controls-module";
+import { WebAudioParam } from "./webaudio-controls-module";
+import { WebAudioKeyboard } from "./webaudio-controls-module";
+
+type CustomEvents<K extends string> = {
+  [key in K]: (event: CustomEvent) => void;
+};
+type CustomElement<T, K extends string = ""> = Partial<
+  T & DOMAttributes<T> & { children: any } & CustomEvents<`on${K}`>
+>;
+
+console.log("WebAudioKnob", WebAudioKnob);
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      ["webaudio-knob"]: CustomElement<WebAudioKnob>;
+      ["webaudio-slider"]: CustomElement<WebAudioSlider>;
+      ["webaudio-switch"]: CustomElement<WebAudioSwitch>;
+      ["webaudio-param"]: CustomElement<WebAudioParam>;
+      ["webaudio-keyboard"]: CustomElement<WebAudioKeyboard>;
+    }
+  }
+}
