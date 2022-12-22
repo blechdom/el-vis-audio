16e40dbd20ca5bde68fc7fd16c963c37a8756210
Kristin Galvin <kgalvin@nvidia.com>
2022-12-22 16:55:14 -0500

    add phase modulation and nicks drumynth-hat

---
+++ src/stories/PMDrumSynthNick.stories.tsx
@@ -0,0 +1,197 @@
+import React, { useState, useCallback, useEffect } from "react";
+import { el, NodeRepr_t } from "@elemaudio/core";
+import { Meta, Story } from "@storybook/react";
+import styled from "styled-components";
+import { core, Button, Oscilloscope, PlayPauseAudio, Slider } from "../.";
+require("events").EventEmitter.defaultMaxListeners = 0;
+
+const Demo = () => {
+  const [playing, setPlaying] = useState(false);
+  const [audioVizData, setAudioVizData] = useState<Array<number>>([]);
+  const [pitch, setPitch] = useState<number>(320);
+  const [tone, setTone] = useState<number>(1000);
+  const [attack, setAttack] = useState<number>(0.1);
+  const [decay, setDecay] = useState<number>(1);
+  const [speed, setSpeed] = useState<number>(1);
+  const [mainVolume, setMainVolume] = useState<number>(0);
+
+  function cycle(freq: NodeRepr_t | number, phaseOffset: NodeRepr_t | number) {
+    let t = el.add(el.phasor(freq, 0), phaseOffset);
+    let p = el.sub(t, el.floor(t));
+
+    return el.sin(el.mul(2 * Math.PI, p));
+  }
+
+  useEffect(() => {
+    if (playing) {
+      const smoothPitch: NodeRepr_t = el.sm(
+        el.const({ key: `pitch`, value: pitch })
+      );
+
+      const smoothAttack: NodeRepr_t = el.sm(
+        el.const({ key: `attack`, value: attack })
+      );
+
+      const smoothDecay: NodeRepr_t = el.sm(
+        el.const({ key: `decay`, value: decay })
+      );
+
+      const smoothTone: NodeRepr_t = el.sm(
+        el.const({ key: `tone`, value: tone })
+      );
+
+      const smoothSpeed: NodeRepr_t = el.sm(
+        el.const({ key: `speed`, value: speed })
+      );
+
+      let m2 = el.noise();
+      let m1 = cycle(el.mul(2, smoothPitch), el.mul(2, m2));
+      let m0 = cycle(smoothPitch, el.mul(2, m1));
+
+      let f = el.bandpass(smoothTone, 1.214, m0);
+
+      let env = el.adsr(
+        smoothAttack,
+        smoothDecay,
+        0.0,
+        0.1,
+        el.train(smoothSpeed)
+      );
+
+      let synth = el.mul(f, env) as NodeRepr_t;
+
+      const scaledSynth = el.mul(
+        synth,
+        el.sm(el.const({ key: `main-amp`, value: mainVolume / 100 }))
+      );
+      core.render(el.scope({ name: "scope" }, scaledSynth), scaledSynth);
+    }
+  }, [mainVolume, core, playing, pitch, tone, attack, decay, speed]);
+
+  function handleScopeData(data: Array<Array<number>>) {
+    if (data.length) {
+      setAudioVizData(data[0]);
+    }
+  }
+
+  core?.on("scope", function (e) {
+    if (e.source === "scope") {
+      handleScopeData(e.data);
+    }
+  });
+
+  return (
+    <>
+      <h1>Nick Thompson's Drumsynth - Hat</h1>
+      <h5>
+        <a
+          href={
+            "https://github.com/nick-thompson/drumsynth/blob/master/hat.js#L5"
+          }
+        >
+          https://github.com/nick-thompson/drumsynth/blob/master/hat.js#L5
+        </a>
+      </h5>
+      <PlayPauseAudio onPlay={setPlaying} />
+      <Oscilloscope
+        audioVizData={audioVizData}
+        color={"#FF0000"}
+        width={400}
+        height={100}
+      />
+      <br />
+      <h2>
+        main volume = <SliderLabel>{mainVolume}</SliderLabel>
+      </h2>
+      <Slider
+        value={mainVolume}
+        min={0}
+        step={0.1}
+        max={100}
+        onChange={(event) => setMainVolume(parseFloat(event.target.value))}
+      />
+      <h2>
+        speed = <SliderLabel>{speed}</SliderLabel>
+      </h2>
+      <Slider
+        value={speed}
+        min={0}
+        step={0.1}
+        max={40}
+        onChange={(event) => setSpeed(parseFloat(event.target.value))}
+      />
+      <h2>
+        frequency = <SliderLabel>{pitch}</SliderLabel>
+      </h2>
+      <Slider
+        value={pitch}
+        step={0.01}
+        min={317}
+        max={3170}
+        onChange={(event) => setPitch(parseFloat(event.target.value))}
+      />
+      <h2>
+        tone = <SliderLabel>{tone}</SliderLabel>
+      </h2>
+      <Slider
+        value={tone}
+        step={0.01}
+        min={800}
+        max={18000}
+        onChange={(event) => setTone(parseFloat(event.target.value))}
+      />
+      <h2>
+        attack = <SliderLabel>{attack}</SliderLabel>
+      </h2>
+      <Slider
+        value={attack}
+        step={0.001}
+        min={0.005}
+        max={0.2}
+        onChange={(event) => setAttack(parseFloat(event.target.value))}
+      />
+      <h2>
+        decay = <SliderLabel>{decay}</SliderLabel>
+      </h2>
+      <Slider
+        value={decay}
+        step={0.001}
+        min={0.005}
+        max={4.0}
+        onChange={(event) => setDecay(parseFloat(event.target.value))}
+      />
+    </>
+  );
+};
+
+const StyledButton = styled(Button)`
+  background-color: #0f9ff5;
+  color: #ffffff;
+  border: none;
+  margin: 0.5em 0.5em 0.5em 0;
+  padding: 0.5em;
+  :hover {
+    background-color: #ffab00;
+    color: #000000;
+  }
+`;
+
+const SliderLabel = styled.span`
+  display: inline-block;
+  width: 150px;
+  text-align: left;
+`;
+
+const Presets = styled.div`
+  margin-right: 25px;
+`;
+
+const meta: Meta = {
+  title: `experiments/Nick Thompson's DrumSynth Hat`,
+  component: Demo,
+};
+export default meta;
+
+const Template: Story = () => <Demo />;
+
+export const Default = Template.bind({});
---
+++ src/stories/PhaseModulation.stories.tsx
@@ -0,0 +1,163 @@
+import React, { useState, useCallback, useEffect } from "react";
+import { el, NodeRepr_t } from "@elemaudio/core";
+import { Meta, Story } from "@storybook/react";
+import styled from "styled-components";
+import { core, Button, Oscilloscope, PlayPauseAudio, Slider } from "../.";
+require("events").EventEmitter.defaultMaxListeners = 0;
+
+const Demo = () => {
+  const [playing, setPlaying] = useState(false);
+  const [audioVizData, setAudioVizData] = useState<Array<number>>([]);
+  const [steps, setSteps] = useState<number>(3);
+  const [indexOfMod, setIndexOfMod] = useState<number>(1);
+  const [carrierFreq, setCarrierFreq] = useState<number>(400);
+  const [startFreq, setStartFreq] = useState<number>(1);
+  const [freqDiv, setFreqDiv] = useState<number>(1);
+  const [ampDiv, setAmpDiv] = useState<number>(1);
+  const [mainVolume, setMainVolume] = useState<number>(0);
+
+  useEffect(() => {
+    if (playing) {
+      const smoothCarrierFreq: NodeRepr_t = el.sm(
+        el.const({ key: `carrierFreq`, value: carrierFreq })
+      );
+      const smoothModulationFreq: NodeRepr_t = el.sm(
+        el.const({ key: `startFreq`, value: startFreq })
+      );
+
+      const indexOfModulation: NodeRepr_t = el.sm(
+        el.const({ key: `indexOfMod`, value: indexOfMod })
+      );
+
+      const synth = el.sin(
+        el.mul(
+          2 * Math.PI,
+          el.add(
+            el.phasor(smoothModulationFreq, 0),
+            el.mod(
+              el.mul(
+                el.sin(el.mul(2 * Math.PI, el.phasor(smoothCarrierFreq, 0))),
+                indexOfModulation,
+                1
+              )
+            )
+          )
+        )
+      );
+
+      const scaledSynth = el.mul(
+        synth,
+        el.sm(el.const({ key: `main-amp`, value: mainVolume / 100 }))
+      );
+      core.render(el.scope({ name: "scope" }, scaledSynth), scaledSynth);
+    }
+  }, [
+    indexOfMod,
+    steps,
+    carrierFreq,
+    startFreq,
+    mainVolume,
+    core,
+    playing,
+    freqDiv,
+    ampDiv,
+  ]);
+
+  function handleScopeData(data: Array<Array<number>>) {
+    if (data.length) {
+      setAudioVizData(data[0]);
+    }
+  }
+
+  core?.on("scope", function (e) {
+    if (e.source === "scope") {
+      handleScopeData(e.data);
+    }
+  });
+
+  return (
+    <>
+      <h1>Phase Modulation</h1>
+      <PlayPauseAudio onPlay={setPlaying} />
+      <Oscilloscope
+        audioVizData={audioVizData}
+        color={"#FF0000"}
+        width={400}
+        height={100}
+      />
+      <br />
+      <h2>
+        main volume = <SliderLabel>{mainVolume}</SliderLabel>
+      </h2>
+      <Slider
+        value={mainVolume}
+        min={0}
+        step={0.1}
+        max={100}
+        onChange={(event) => setMainVolume(parseFloat(event.target.value))}
+      />
+      <h2>
+        carrier frequency = <SliderLabel>{carrierFreq}</SliderLabel>
+      </h2>
+      <Slider
+        value={carrierFreq}
+        step={0.01}
+        min={0}
+        max={1200}
+        onChange={(event) => setCarrierFreq(parseFloat(event.target.value))}
+      />
+      <h2>
+        modulator frequency = <SliderLabel>{startFreq}</SliderLabel>
+      </h2>
+      <Slider
+        value={startFreq}
+        step={0.01}
+        min={0}
+        max={400}
+        onChange={(event) => setStartFreq(parseFloat(event.target.value))}
+      />
+      <h2>
+        index of modulation = <SliderLabel>{indexOfMod}</SliderLabel>
+      </h2>
+      <Slider
+        value={indexOfMod}
+        step={0.01}
+        min={0}
+        max={20}
+        onChange={(event) => setIndexOfMod(parseFloat(event.target.value))}
+      />
+    </>
+  );
+};
+
+const StyledButton = styled(Button)`
+  background-color: #0f9ff5;
+  color: #ffffff;
+  border: none;
+  margin: 0.5em 0.5em 0.5em 0;
+  padding: 0.5em;
+  :hover {
+    background-color: #ffab00;
+    color: #000000;
+  }
+`;
+
+const SliderLabel = styled.span`
+  display: inline-block;
+  width: 150px;
+  text-align: left;
+`;
+
+const Presets = styled.div`
+  margin-right: 25px;
+`;
+
+const meta: Meta = {
+  title: "experiments/Phase Modulation",
+  component: Demo,
+};
+export default meta;
+
+const Template: Story = () => <Demo />;
+
+export const Default = Template.bind({});
