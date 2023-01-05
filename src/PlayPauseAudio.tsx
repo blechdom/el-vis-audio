6113d709eecfaaaee05a1a65845e5d9d69cb35a4
Kristin Galvin <kgalvin@nvidia.com>
2023-01-05 15:21:17 -0800

    revert play pause audio

--- src/PlayPauseAudio.tsx
+++ src/PlayPauseAudio.tsx
@@ -1,28 +1,22 @@
-import React, { FC, useCallback, useEffect, useState } from "react";
+import React, { FC, useState } from "react";
+import { audioContext } from "./utils/audioContext";
 import { PlayPauseButton } from "./PlayPauseButton";
 import { PlayPauseAudioProps } from "./PlayPauseAudio.types";
-import { audioContext, core } from "./";
 
 export const PlayPauseAudio: FC<PlayPauseAudioProps> = ({
   playing = false,
-  signal,
   onPlay,
   ...props
 }: PlayPauseAudioProps) => {
   const [contextPlaying, setContextPlaying] = useState<boolean>(false);
 
-  useEffect(async () => {
-    console.log("contextPlaying", contextPlaying);
-    onPlay && onPlay(contextPlaying);
-    if (!contextPlaying) {
-      await audioContext.suspend();
-    } else {
-      await audioContext.resume();
-      signal && core && core.render(signal, signal);
-    }
-  }, [contextPlaying, signal, core]);
-
   const togglePlay = () => {
+    if (contextPlaying) {
+      audioContext.suspend();
+    } else {
+      audioContext.resume();
+    }
+    onPlay && onPlay(!contextPlaying);
     setContextPlaying((play) => !play);
   };
 
